80e811ab7cf399247e5054cf6556d3b19d1dd053
[dyad] <suheldhimal104@gmail.com>
2026-03-01 11:17:30 +0000

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { CompareProvider } from "./contexts/CompareContext";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <CompareProvider>
+    <App />
+  </CompareProvider>
+);
